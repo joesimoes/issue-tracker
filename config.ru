a69994a95ff1c9337bdd832b468b53cd3baaff8b
Joe Simoes <joseph.simoes@gmail.com>
2016-08-19 13:35:33 -0400

    Add issue model, set up tests

--- config.ru
+++ config.ru
@@ -1,3 +1,4 @@
+require "./env"
 require "./lib/app"
 
 run App
--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,12 @@
+require_relative "../lib/models/issue"
+
 class App < Sinatra::Base
   get "/" do
     "Hello" 
   end
+	
+	get "/issues" do
+		@issues = Issue.all
+		@issues
+	end
 end
