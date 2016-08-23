85c46cbb037536b41ef4b9518db9307b1174b6dd
Joe Simoes <joseph.simoes@gmail.com>
2016-08-22 22:53:05 -0400

    Create issues

--- lib/app.rb
+++ lib/app.rb
@@ -12,4 +12,16 @@ class App < Sinatra::Base
     @issues = Issue.all
     haml :"issues/index"
   end
+
+  get "/issues/new" do
+    @issue = Issue.new
+    haml :"issues/new"
+  end
+
+  post "/issues" do
+    @issue = Issue.new params[:issue]
+    if @issue.save
+      redirect "/"
+    end
+  end
 end
